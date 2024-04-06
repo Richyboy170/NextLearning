1557e67c8abcdd1d331b235046994dbfc355c6ff
Richyboy170 <richyboy170@gmail.com>
2024-04-06 16:06:04 +0700

    3

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 export default function Page() {
     return (
-      <p>Dashboard Pagel</p>
+      <p>Dashboard Page</p>
     );
   }
--- app/ui/lib/definitions.ts
+++ app/ui/lib/definitions.ts
@@ -12,7 +12,7 @@ export type User = {
 export type Customer = {
   id: string;
   name: string;
-  email: string;
+  email: string;  
   image_url: string;
 };
 
